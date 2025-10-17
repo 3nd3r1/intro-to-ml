1431be59a00fe06ed5ae73fb3bbd767c9d27e38e
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-17 16:42:12 +0300

    exercise-0: make a dummy plot script

---
+++ exercise-0/1-plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 5, 4, 6]
+
+# Create scatterplot
+plt.scatter(x, y)
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.title('Simple Scatterplot')
+plt.show()
