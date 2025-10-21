4a26e20355d800e8298f153eb4b7d8f97b461c33
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-21 13:21:16 +0300

    exercise-0: finalise 0-1

--- exercise-0/1-plot.py
+++ exercise-0/1-plot.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv("./x.csv")
-variables = df.var().sort_values(ascending=False).take([0, 1]).keys().to_list()
+data = pd.read_csv("./x.csv")
+variances = data.select_dtypes(include=[np.number]).var()
+if type(variances) is not pd.Series or len(variances) < 2:
+    raise ValueError("Invalid data")
 
-df.plot(x=variables[0], y=variables[1], kind="scatter")
+two_vars = variances.sort_values(ascending=False).take([0, 1]).keys().to_list()
+
+data.plot(x=two_vars[0], y=two_vars[1], kind="scatter")
 plt.show()
