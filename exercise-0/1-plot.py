2bcac7a85aa1acbb3c74033365f13f1b73a8c2e2
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-21 12:01:09 +0300

    exercise-0: add some nix magic

--- exercise-0/1-plot.py
+++ exercise-0/1-plot.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 df = pd.read_csv("./x.csv")
-variables = df.var().sort_values(ascending=False).take([0,1]).keys().to_list()
+variables = df.var().sort_values(ascending=False).take([0, 1]).keys().to_list()
 
 df.plot(x=variables[0], y=variables[1], kind="scatter")
 plt.show()
