624affeb460e6b252653ca2a4590e799b566faf3
3nd3r1 <ender.spam.guru@gmail.com>
2025-11-12 09:20:41 +0200

    exercise-1: finnish p5-b

---
+++ exercise-1/p5/p5-b.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+
+
+results = []
+
+plt.subplots(2, 2, figsize=(10, 8))
+for i in range(1, 5):
+    data = pd.read_csv(f"d{i}.csv")
+    model = sm.OLS(data["y"], sm.add_constant(data["x"])).fit()
+
+    plt.subplot(2, 2, i)
+    plt.scatter(data["x"], data["y"], label="Data Points")
+    plt.plot(data["x"], model.fittedvalues, color="red", label="Fitted Line")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(f"Dataset d{i}.csv")
+    plt.grid(True, alpha=0.3)
+
+plt.tight_layout()
+plt.savefig("p5-b.png")
+plt.show()
