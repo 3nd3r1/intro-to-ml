91f187fc947feb341cb508969c9dd0287c0e301f
3nd3r1 <ender.spam.guru@gmail.com>
2025-11-19 13:23:48 +0200

    exercise-2: finish p10-a

--- exercise-2/p10/p10-a.py
+++ exercise-2/p10/p10-a.py
@@ -4,6 +4,13 @@ import pandas as pd
 df_train = pd.read_csv("penguins_train.csv").dropna()
 df_test = pd.read_csv("penguins_test.csv").dropna()
 
+stats = df_train.groupby("species").agg(["mean", "std"]).T
+stats.columns.name = None
+stats = stats.reset_index()
+stats.columns = ["Feature", "Statistic", "Adelie", "notAdelie"]
 
-stats = df_train.groupby("species").agg(["mean", "std"]).reset_index(drop=True)
-print(stats)
+class_counts = df_train["species"].value_counts()
+class_probs = (class_counts + 1) / (len(df_train) + 2)
+
+print(stats.to_latex(escape=True, index=False, float_format="%.4f"))
+print(class_probs.to_latex(escape=True, index=True, float_format="%.4f"))
